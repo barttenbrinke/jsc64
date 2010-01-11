e889b50b0e49f88fba4f609f63d8cdfe0722b8e9
Bart ten Brinke <info@retrosync.com>
2010-01-11 23:43:55 +0100

    Added color caching

--- js/nl/kingsquare/as3/flash/display/BitmapData.js
+++ js/nl/kingsquare/as3/flash/display/BitmapData.js
@@ -19,43 +19,66 @@
  */
 
 nl.kingsquare.as3.flash.display.BitmapData = Class.extend({
+    
     init: function(context) {
         this.context = context;
+        this.color_lookup_string = {};
+        this.color_lookup_array = {};
+        this.pixelData = null;
+        this.pixelDataColorCache = null;
+    },
+    
+    cachedColorString: function(color) {
+      if (this.color_lookup_string[color] == null) {
+        this.color_lookup_string[color] = 'rgba('+((color >> 16) & 0xFF) +
+                ','+((color >> 8) & 0xFF)+','+(color & 0xFF)+
+                ', '+((color >> 24) & 0xFF)/255+')';
+      }
+      
+      return this.color_lookup_string[color];
+    },
+    
+    cachedColorArray: function(color) {
+      if (this.color_lookup_array[color] == null) {
+        this.color_lookup_array[color] = [((color >> 16) & 0xFF), ((color >> 8) & 0xFF), (color & 0xFF), ((color >> 24) & 0xFF)];
+      }
+
+      return this.color_lookup_array[color];
     },
 
     fillRect: function(rect/*:Rectangle*/, color/*:uint*/) {
-    	this.context.clearRect(rect.x, rect.y, rect.width, rect.height);
-    	this.context.fillStyle = 'rgba('+((color >> 16) & 0xFF) +
-    			','+((color >> 8) & 0xFF)+','+((color >> 0) & 0xFF)+
-    			', '+((color >> 24) & 0xFF)/255+')';
-        this.context.fillRect(rect.x, rect.y, rect.width, rect.height);
+      this.context.clearRect(rect.x, rect.y, rect.width, rect.height);
+      this.context.fillStyle = this.cachedColorString(color);
+      this.context.fillRect(rect.x, rect.y, rect.width, rect.height);
     },
 
     setPixel32: function(x/*:int*/, y/*:int*/, color/*:uint*/) {
-    	var pixelData;
-
     	if (typeof color == 'undefined') color = 0;
     	if (typeof x == 'undefined') x = 0;
     	if (typeof y == 'undefined') y = 0;
 
-		// Not all browsers implement createImageData. On such browsers we obtain the
-		// ImageData object using the getImageData method. The worst-case scenario is
-		// to create an object *similar* to the ImageData object and hope for the best
-		// luck.
-		if (this.context.createImageData) {
-			pixelData = this.context.createImageData(1, 1);
-		} else if (this.context.getImageData) {
-			pixelData = this.context.getImageData(x, y, 1, 1);
-		} else {
-			pixelData = {'width' : 1, 'height' : 1, 'data' : new Array(4)};
-		}
-
-		pixelData.data[0] = (color >> 16) & 0xFF //R
-		pixelData.data[1] = (color >> 8) & 0xFF //G
-		pixelData.data[2] = (color) & 0xFF //B
-		pixelData.data[3] = (color >> 24) & 0xFF //A
-
-		// Draw the ImageData object.
-		this.context.putImageData(pixelData, x, y);
-	}
+  		// Not all browsers implement createImageData. On such browsers we obtain the
+  		// ImageData object using the getImageData method. The worst-case scenario is
+  		// to create an object *similar* to the ImageData object and hope for the best
+  		// luck.
+  		if (this.pixelData == null) {
+    		if (this.context.createImageData) {
+    			this.pixelData = this.context.createImageData(1, 1);
+    		} else if (this.context.getImageData) {
+    			this.pixelData = this.context.getImageData(x, y, 1, 1);
+    		} else {
+    			this.pixelData = {'width' : 1, 'height' : 1, 'data' : new Array(4)};
+    		}
+    	}
+		
+  		this.pixelDataColorCache = this.cachedColorArray(color);
+		
+      this.pixelData.data[0] = this.pixelDataColorCache[0];
+      this.pixelData.data[1] = this.pixelDataColorCache[1];
+      this.pixelData.data[2] = this.pixelDataColorCache[2];
+      this.pixelData.data[3] = this.pixelDataColorCache[3];
+        
+  		// Draw the ImageData object.
+  		this.context.putImageData(this.pixelData, x, y);
+  	}
 });
